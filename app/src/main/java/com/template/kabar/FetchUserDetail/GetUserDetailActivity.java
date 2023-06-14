27fa990544773e15e210ecf03bd1a5a40f8f769b
Ayush-aidash <ayush.rawat@aidash.com>
2023-06-14 20:28:01 +0530

    Autologin applied with bottom navigation menu colored

---
+++ app/src/main/java/com/template/kabar/FetchUserDetail/GetUserDetailActivity.java
@@ -0,0 +1,16 @@
+package com.template.kabar.FetchUserDetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.template.kabar.R;
+
+public class GetUserDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_user_detail);
+    }
+}
